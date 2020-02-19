9f996dccd993e5fdcda2e62d23e058a54a07a88b
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-19 10:50:23 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts "The first letter in the alphabet is: #{colors[0][0].to uppercase}"
+  puts "The first letter in the alphabet is: #{colors[1][0].to uppercase}"
+  puts "The first letter in the alphabet is: #{colors[2][0].to uppercase}"
+  puts "The first letter in the alphabet is: #{colors[3][0].to uppercase}"
+  puts "The first letter in the alphabet is: #{colors[4][0].to uppercase}"
+  puts "The first letter in the alphabet is: #{colors[5][0].to uppercase}"
+  puts "The first letter in the alphabet is: #{colors[6][0].to uppercase}"
+end
